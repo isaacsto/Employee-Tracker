ac9ef44815e587ac0045b1fcd6ff8c5d72892ed6
Isaac Stofko <isaacstofko@gmail.com>
2023-04-12 19:54:53 -0400

    removed express

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-//require express
-const express = require('express');
+
 // Import and require mysql2
 const mysql = require('mysql2');
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
