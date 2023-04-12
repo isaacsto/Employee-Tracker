5bdc1b5336cc6052f6e6c784d7be811faf6fe116
Isaac Stofko <isaacstofko@gmail.com>
2023-04-12 16:34:24 -0400

    inquirer added

--- server.js
+++ server.js
@@ -31,9 +31,34 @@ inquirer
     {
       type: 'list',
       name: 'question',
-      message: '?',
-      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employy role']
+      message: 'What would you like to do?',
+      choices: [
+      'View all departments', 
+      'View all roles', 
+      'View all employees', 
+      'Add a department', 
+      'Add a role', 
+      'Add an employee', 
+      'Update an employee role'
+    ]
     },
 ])
 
-.then
+.then((answers) => {
+    switch(answers.action) {
+        case 'View all departments':
+            break;
+        case 'View all roles':
+            break;
+        case 'View all employees':
+            break;
+        case 'Add a department':
+            break;
+        case 'Add a role':
+            break; 
+        case 'Add an employee':
+            break;
+        case 'Update an employee role':
+            break;
+    }
+})
