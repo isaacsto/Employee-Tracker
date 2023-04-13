06ed22b35caff3b72e83104ecc2a2b413e9aff05
Isaac Stofko <isaacstofko@gmail.com>
2023-04-13 18:19:12 -0400

    syntax

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-
 const mysql = require('mysql2');
-
 const inquirer = require('inquirer');
-const fs = require('fs');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
 
 const db = mysql.createConnection(
   {
@@ -17,47 +11,46 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db database.`)
 );
 
-
-inquirer
-  .prompt([
+async function main() {
+  const answers = await inquirer.prompt([
     {
       type: 'list',
-      name: 'question',
+      name: 'action',
       message: 'What would you like to do?',
       choices: [
-      'View all departments', 
-      'View all roles', 
-      'View all employees', 
-      'Add a department', 
-      'Add a role', 
-      'Add an employee', 
-      'Update an employee role'
-    ]
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role'
+      ]
     },
-])
+  ]);
 
+  switch (answers.action) {
+    case 'View all departments':
+      const [departments] = await db.execute('SELECT * FROM departments');
+      console.table(departments);
+      break;
+    case 'View all roles':
+      const [roles] = await db.execute('SELECT * FROM roles');
+      console.table(roles);
+      break;
+    case 'View all employees':
+      const [employees] = await db.execute('SELECT * FROM employees');
+      console.table(employees);
+      break;
+    case 'Add a department':
+      break;
+    case 'Add a role':
+      break;
+    case 'Add an employee':
+      break;
+    case 'Update an employee role':
+      break;
+  }
+}
 
-.then(async (answers) => {
-    switch(answers.action) {
-        case 'View all departments':
-            const [departments] = await db.execute('SELECT * FROM departments')
-            console.table(departments);
-            break;
-        case 'View all roles':
-            const [roles] = await db.execute('SELECT * FROM roles');
-            console.table(roles);
-            break;
-        case 'View all employees':
-            const [employees] = await database.execute('SELECT * FROM employees');
-            console.table(employees);
-            break;
-        case 'Add a department':
-            break;
-        case 'Add a role':
-            break; 
-        case 'Add an employee':
-            break;
-        case 'Update an employee role':
-            break;
-    }
-});
+main();
