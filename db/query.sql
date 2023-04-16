66a6556384994817814413d95510e4116e144e6f
Isaac Stofko <isaacstofko@gmail.com>
2023-04-16 12:05:20 -0400

    quicksave

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,4 @@
 SELECT employees.employee_name AS name, employee_roles.title AS title 
 FROM employees 
-JOIN employees_roles ON employees.role_id = employee_roles.id 
+JOIN employee_roles ON employee.role_id = employee_roles.id 
 JOIN departments ON employee_roles.department_id = departments.id; 
